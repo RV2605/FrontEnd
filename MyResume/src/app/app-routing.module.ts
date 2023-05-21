4d78db24ed3d064a2dbdead00e254fb41d3422ca
RV2605 <rvkhanna2605@gmail.com>
2023-05-20 21:24:13 -0400

    Routing added

--- MyResume/src/app/app-routing.module.ts
+++ MyResume/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LinkedinComponent } from './linkedin/linkedin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:LinkedinComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MyResume/src/app/app.component.ts
+++ MyResume/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Vinoth K Ravichelvam';
+  linkedin(){
+    alert('profile shared');
+  }
 }
